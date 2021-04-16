b20ba9257a236136be851f4f1c973cfa187a90cf
Rafael Correa <rafael_c_alves@hotmail.com>
2021-04-15 23:20:37 -0300

    Added Springboot structure with home hello world page

---
+++ src/main/java/com/unisinos/gerenciarsaofrancisco/GerenciarSaoFranciscoApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.gerenciarsaofrancisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarSaoFranciscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciarSaoFranciscoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unisinos/gerenciarsaofrancisco/GerenciarSaoFranciscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.gerenciarsaofrancisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarSaoFranciscoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
