4644622a530ea86712141d8be16b446c4686d92b
Rafael Correa <rafael.correa@sap.com>
2021-06-04 08:38:49 -0300

    Added @component - bean ref - to populator

--- src/main/java/com/unisinos/gerenciarsaofrancisco/populators/impls/MedicalPrescriptionPopulator.java
+++ src/main/java/com/unisinos/gerenciarsaofrancisco/populators/impls/MedicalPrescriptionPopulator.java
@@ -3,7 +3,9 @@ package com.unisinos.gerenciarsaofrancisco.populators.impls;
 import com.unisinos.gerenciarsaofrancisco.forms.MedicalPrescriptionForm;
 import com.unisinos.gerenciarsaofrancisco.models.MedicalPrescription;
 import com.unisinos.gerenciarsaofrancisco.populators.Populator;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MedicalPrescriptionPopulator implements Populator<MedicalPrescriptionForm,MedicalPrescription> {
     @Override
     public void populate(MedicalPrescriptionForm source, MedicalPrescription target) {
